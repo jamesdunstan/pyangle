fff42d32657e4b75b3bac7d5d75d691e84e1efc7
jamesdunstan <74322910+jamesdunstan@users.noreply.github.com>
2025-03-06 14:13:00 +0000

    add test functionality

---
+++ src/pyangle/calc.py
@@ -0,0 +1,23 @@
+### module to calculate the hypotenuse of a right angle triangle. need numpy for square root. 
+import numpy as np
+
+## function 1 to calculate the square of a variable.
+# Inputs: a
+def square(a, b):
+    a2 = a**2
+    b2 = b**2
+    return(a2, b2)
+# Outputs: a**2
+
+## function 2 that calculates  square root of a**2 + b**2 using function 1. 
+# Inputs: a and b
+def hypot(a,b):
+    c2 = a + b
+    c = np.sqrt(c2)
+    return(c)
+# outputs: c
+
+def pythag(a,b):
+    a2, b2 = square(a,b)
+    hypotenuse = hypot(a2,b2)
+    return hypotenuse
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,15 +1,48 @@
-def test_example(self):
+import pytest
+import numpy as np
+import pyangle.calc
+
+def test_square():
     '''Test for the example function'''
 
     # Arrange
-    test_variable_1 = 0
+    test_variable_1 = 1
     test_variable_2 = 1
-    expected_output = 7
+    expected_output_1 = 1
+    expected_output_2 = 1
 
     # Act
-    output = your_function(test_variable_1, test_variable_2)
+    output_1, output_2 = pyangle.calc.square(test_variable_1, test_variable_2)
 
     # Assert
-    assert output == expected_output
+    assert output_1 == expected_output_1
+    assert output_2 == expected_output_2
 
-    # No cleanup needed
+def test_hypot():
+    
+    # Arrange
+    test_a = 1
+    test_b = 1
+    test_c = 1.414
+    
+    # Act
+    
+    hypot_output = pyangle.calc.hypot(test_a, test_b)
+    
+    # Assert
+    
+    assert test_c == pytest.approx(hypot_output, abs=1e-3)
+    
+def test_pythag():
+    
+    # Arrange
+    test_a = 2
+    test_b = 3
+    test_c = 3.6056
+    
+    # Act
+    pythag_output = pyangle.calc.pythag(test_a,test_b)
+    
+    # Assert
+    
+    assert test_c == pytest.approx(pythag_output, abs=1e3)
